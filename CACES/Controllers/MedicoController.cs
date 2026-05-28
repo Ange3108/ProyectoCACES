ae18e0cd3b6a3139baf950e05792f8c69369cc14
Alvarado-Majo <mariajosealvarado235@gmail.com>
2026-05-28 08:29:43 -0600

    Editar medico
    Se crea la interfas de editar medico

---
+++ CACES/Controllers/MedicoController.cs
@@ -0,0 +1,53 @@
+using CACES.BLL.Servicios.Medicos;
+using CACES.DAL.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CACES.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicoController : ControllerBase
+    {
+        private readonly IMedicoServicio _medicoServicio;
+
+        public MedicoController(IMedicoServicio medicoServicio)
+        {
+            _medicoServicio = medicoServicio;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMedicos()
+        {
+            var lista = await _medicoServicio.GetMedicosAsync();
+
+            return Ok(lista);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMedicoById(int id)
+        {
+            var medico = await _medicoServicio.GetMedicoByIdAsync(id);
+
+            if (medico == null)
+                return NotFound();
+
+            return Ok(medico);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMedico(int id, [FromBody] Medico medico)
+        {
+            if (id != medico.IdMedico)
+            {
+                return BadRequest("El ID no coincide");
+            }
+
+            var result = await _medicoServicio.UpdateMedicoAsync(medico);
+
+            if (!result)
+                return NotFound();
+
+            return Ok();
+        }
+    }
+    ]
