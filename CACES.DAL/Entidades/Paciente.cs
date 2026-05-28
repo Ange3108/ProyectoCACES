ae3086562b0a2c016428027d223bb64063dad83d
Jeff-repo <jgranados80082@ufide.ac.cr>
2026-05-28 11:12:20 -0600

    sincronizando con main

--- CACES.DAL/Entidades/Paciente.cs
+++ CACES.DAL/Entidades/Paciente.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CACES.DAL.Entidades
 {
+
+    [Table("Pacientes")]
     public partial class Paciente
     {
+        [Key]
+        [Column("Id_Paciente")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPaciente { get; set; }
+
+        [Required(ErrorMessage = "El usuario es requerido")]
+        [Column("Id_Usuario")]
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El historial médico es requerido")]
+        [Column("Id_Historial")]
+        [ForeignKey("HistorialMedico")]
+        public int IdHistorial { get; set; }
+
+        public virtual Usuario Usuario { get; set; } = null!;
+
+        public virtual HistorialMedico HistorialMedico { get; set; } = null!;
+
+
 
     }
 }
--- CACES.DAL/Repositorios/Medicos/MedicoRepositorio.cs
+++ CACES.DAL/Repositorios/Medicos/MedicoRepositorio.cs
@@ -1,10 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CACES.DAL.DBContext;
+using CACES.DAL.Entidades;
+using Microsoft.EntityFrameworkCore;
 
-namespace CACES.DAL.Repositorios.Médicos
+namespace CACES.DAL.Repositorios.Medicos
 {
-    internal class MedicoRepositorio
+    public class MedicoRepositorio : IMedicoRepositorio
     {
+        private readonly CACESDbContext _context;
+
+        public MedicoRepositorio(CACESDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Medico>> GetMedicosAsync()
+        {
+            return await _context.Medicos.ToListAsync();
+        }
+
+        public async Task<Medico> GetMedicoByIdAsync(int id)
+        {
+            return await _context.Medicos
+                .FirstOrDefaultAsync(x => x.IdMedico == id);
+        }
+
+        public async Task<bool> CreateMedicoAsync(Medico medico)
+        {
+            await _context.Medicos.AddAsync(medico);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateMedicoAsync(Medico medico)
+        {
+            var existing = await _context.Medicos
+                .FirstOrDefaultAsync(x => x.IdMedico == medico.IdMedico);
+
+            if (existing == null)
+                return false;
+
+            existing.Nombre = medico.Nombre;
+            existing.Especialidad = medico.Especialidad;
+            existing.Experiencia = medico.Experiencia;
+            existing.Correo = medico.Correo;
+            existing.Telefono = medico.Telefono;
+            existing.Descripcion = medico.Descripcion;
+            existing.Estado = medico.Estado;
+            existing.Foto = medico.Foto;
+
+            _context.Medicos.Update(existing);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteMedicoAsync(int id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+
+            if (medico == null)
+                return false;
+
+            _context.Medicos.Remove(medico);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
 }
