810af3db3f315e433ee4e696925cf4e202cf3144
Alvarado-Majo <mariajosealvarado235@gmail.com>
2026-05-30 14:03:54 -0600

    repositori y servicio de Paciente

--- CACES.BLL/Servicios/Paciente/IPacienteServicio.cs
+++ CACES.BLL/Servicios/Paciente/IPacienteServicio.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CACES.DAL.Entidades;
 
 namespace CACES.BLL.Servicios.Paciente
 {
     public interface IPacienteServicio
     {
+        Task<List<DAL.Entidades.Paciente>> GetPacientesAsync();
+
+        Task<DAL.Entidades.Paciente> GetPacienteByIdAsync(int id);
+
+        Task<DAL.Entidades.Paciente> GetPacienteByDUIAsync(string dui);
+
+        Task<bool> CreatePacienteAsync(
+            DAL.Entidades.Paciente paciente);
+
+        Task<bool> UpdatePacienteAsync(
+            DAL.Entidades.Paciente paciente);
+
+        Task<bool> DeletePacienteAsync(int id);
     }
 }
--- CACES.BLL/Servicios/Paciente/PacienteServicio.cs
+++ CACES.BLL/Servicios/Paciente/PacienteServicio.cs
@@ -1,10 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CACES.DAL.Entidades;
+using CACES.DAL.Repositorios.Pacientes;
 
 namespace CACES.BLL.Servicios.Paciente
 {
-    public class PacienteServicio
+    public class PacienteServicio : IPacienteServicio
     {
+        private readonly IPacienteRepositorio _pacienteRepositorio;
+
+        public PacienteServicio(
+            IPacienteRepositorio pacienteRepositorio)
+        {
+            _pacienteRepositorio = pacienteRepositorio;
+        }
+
+        public async Task<List<DAL.Entidades.Paciente>>
+            GetPacientesAsync()
+        {
+            return await _pacienteRepositorio
+                .GetPacientesAsync();
+        }
+
+        public async Task<DAL.Entidades.Paciente>
+            GetPacienteByIdAsync(int id)
+        {
+            return await _pacienteRepositorio
+                .GetPacienteByIdAsync(id);
+        }
+
+        public async Task<DAL.Entidades.Paciente>
+            GetPacienteByDUIAsync(string dui)
+        {
+            return await _pacienteRepositorio
+                .GetPacienteByDUIAsync(dui);
+        }
+
+        public async Task<bool>
+            CreatePacienteAsync(
+            DAL.Entidades.Paciente paciente)
+        {
+            return await _pacienteRepositorio
+                .CreatePacienteAsync(paciente);
+        }
+
+        public async Task<bool>
+            UpdatePacienteAsync(
+            DAL.Entidades.Paciente paciente)
+        {
+            return await _pacienteRepositorio
+                .UpdatePacienteAsync(paciente);
+        }
+
+        public async Task<bool>
+            DeletePacienteAsync(int id)
+        {
+            return await _pacienteRepositorio
+                .DeletePacienteAsync(id);
+        }
     }
 }
