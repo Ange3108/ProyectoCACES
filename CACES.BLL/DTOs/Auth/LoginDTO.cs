57d5e783c7c72d177af183ac6b2a5b7a5d257342
Alvarado-Majo <mariajosealvarado235@gmail.com>
2026-06-02 14:44:17 -0600

    Finalizacion del Login

--- CACES.BLL/DTOs/Auth/LoginDTO.cs
+++ CACES.BLL/DTOs/Auth/LoginDTO.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace CACES.BLL.DTOs.Auth
 {
-    internal class LoginDTO
+    public class LoginDTO
     {
+        [Required(ErrorMessage = "Debe ingresar el correo electrónico")]
+        [EmailAddress(ErrorMessage = "Correo electrónico inválido")]
+        public string CorreoElectronico { get; set; } = null!;
+
+        [Required(ErrorMessage = "Debe ingresar la contraseña")]
+        public string Password { get; set; } = null!;
     }
 }
--- CACES.BLL/Servicios/Auth/IAuthServicio.cs
+++ CACES.BLL/Servicios/Auth/IAuthServicio.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CACES.BLL.DTOs.Auth;
 
 namespace CACES.BLL.Servicios.Auth
 {
-    internal class IAuthServicio
+    public interface IAuthServicio
     {
+        Task<bool> LoginAsync(LoginDTO dto);
     }
 }
