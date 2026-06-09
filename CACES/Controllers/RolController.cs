244e458aba83fa77b372ea4ff23142554f196ead
Alvarado-Majo <mariajosealvarado235@gmail.com>
2026-06-08 18:34:36 -0600

    Vistas medicos, login y controladores auth y rol

---
+++ CACES/Controllers/RolController.cs
@@ -0,0 +1,41 @@
+﻿using CACES.BLL.Servicios.Roles;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CACES.Controllers
+{
+
+    public class RolController : Controller
+    {
+        private readonly IRolServicio _rolServicio;
+
+        public RolController(IRolServicio rolServicio)
+        {
+            _rolServicio = rolServicio;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GestionRoles()
+        {
+            var usuarios = await _rolServicio.GetUsuariosConRolesAsync();
+            var roles = await _rolServicio.GetRolesAsync();
+
+            ViewBag.Roles = roles;
+
+            return View(usuarios);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CambiarRol(string userId, string roleId)
+        {
+            var resultado = await _rolServicio.CambiarRolAsync(userId, roleId);
+
+            if (resultado)
+                TempData["Mensaje"] = "El rol del usuario se actualizó correctamente.";
+            else
+                TempData["Error"] = "No se pudo actualizar el rol del usuario.";
+
+            return RedirectToAction("GestionRoles");
+        }
+    }
+}
