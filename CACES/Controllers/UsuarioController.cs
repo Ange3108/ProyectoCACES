f9ace9ac328e4a5c7a91ff3fe86033fecfb05593
Jeff-repo <jgranados80082@ufide.ac.cr>
2026-06-16 16:46:19 -0600

    sincronizando

--- CACES/Controllers/UsuarioController.cs
+++ CACES/Controllers/UsuarioController.cs
@@ -21,6 +21,7 @@ namespace CACES.Controllers
             return View("~/Views/Usuarios/RegistroUsuario.cshtml");
         }
 
+
         //Registro
         [HttpPost]
         public async Task<IActionResult> RegistroUsuario(RegistrarUsuarioDTO registrarUsuarioDTO)
